9cb0319a06cbba882f26dfbc1e690dfbc0602bcf
John Roberts <roberts.john@gmail.com>
2025-03-13 19:13:54 -0700

    initial setup

--- src/server/routes/root.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-router.get("/", (request, response) => {
-  response.send("Hello World from route in another file!");
-});
-
-export default router;
