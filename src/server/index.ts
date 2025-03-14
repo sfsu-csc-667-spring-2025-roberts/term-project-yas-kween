57f6cda3546b5b92c301e211826bcd910476c22c
John Roberts <roberts.john@gmail.com>
2025-03-13 20:08:46 -0700

    finished setting up initial application framework

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,19 @@
 import * as path from "path";
-import express from "express";
 
+import express from "express";
 import httpErrors from "http-errors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
 
 import rootRouter from "./routes/root";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(process.cwd(), "public")));
 
