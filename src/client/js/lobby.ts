25ccebb2bbff7ffec0d5095e4b5b14bf1814f8ef
John Roberts <roberts.john@gmail.com>
2025-04-24 20:00:54 -0700

    created game creation form and toggle

---
+++ src/client/js/lobby.ts
@@ -0,0 +1,19 @@
+const createGameButton = document.querySelector("#create-game-button");
+const createGameContainer = document.querySelector("#create-game-container");
+const closeButton = document.querySelector("#close-create-game-form");
+
+createGameButton?.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  createGameContainer?.classList.add("visible");
+});
+
+closeButton?.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  createGameContainer?.classList.remove("visible");
+});
+
+createGameContainer?.addEventListener("click", () => {
+  createGameContainer?.classList.remove("visible");
+});
